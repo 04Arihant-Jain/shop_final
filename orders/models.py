74154c51a1590f052be05be6574fec679b1cc8e1
04Arihant-Jain <ajain2466@gmail.com>
2020-06-27 13:06:24 +0530

    added 1.django all-auth(Google Authentication) 2. warehousemodel with separete zone functionality

---
+++ orders/models.py
@@ -0,0 +1,78 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from shop.models import Product
+
+CITY_CHOICES = (
+    ('Ahmedabad', 'Ahmedabad'),
+    ('Bengaluru', 'Bengaluru'),
+    ('Chennai', 'Chennai'),
+    ('Chittorgarh', 'Chittorgarh'),
+    ('Jodhpur', 'Jodhpur'),
+    ('Raipur', 'Raipur'),
+    ('Sirohi', 'Sirohi'),
+    ('Pali', 'Pali'),
+    ('Delhi', 'Delhi'),
+    ('Hyderabad', 'Hyderabad'),
+    ('Kanpur', 'Kanpur'),
+    ('Kolkata', 'Kolkata'),
+    ('Mumbai', 'Mumbai'),
+    ('Ratnagiri', 'Ratnagiri'),
+    ('Pune', 'Pune'),
+    ('Surat', 'Surat'),
+    ('Sultanpur', 'Sultanpur'),
+    ('Lucknow', 'Lucknow'),
+    ('Patna', 'Patna'),
+    ('Indore', 'Indore'),
+    ('Vadodara', 'Vadodara'),
+    ('Bhopal', 'Bhopal'),
+    ('Coimbatore', 'Coimbatore'),
+    ('Agra', 'Agra'),
+    ('Meerut', 'Meerut'),
+    ('Madurai', 'Madurai'),
+    ('Guwahati', 'Guwahati'),
+    ('Thiruvananthapuram', 'Thiruvananthapuram'),
+    ('Tiruchchirappalli', 'Tiruchchirappalli'),
+    ('Kota', 'Kota'),
+    ('Jammu', 'Jammu'),
+    ('Mangalore', 'Mangalore'),
+    ('Ajmer', 'Ajmer'),
+    ('Shillong', 'Shillong'),
+    ('New Delhi', 'New Delhi')
+)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', null=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100, choices=CITY_CHOICES, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def _str_(self):
+        return 'Order {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.item.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def _str_(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
